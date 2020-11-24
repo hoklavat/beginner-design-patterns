50a36b125df39693b0dfc11340c544efbb46c05c
hoklavat <barisyildiz1982@gmail.com>
2020-11-24 17:01:35 +0300

    .

---
+++ 25-NullObject.cpp
@@ -0,0 +1,37 @@
+﻿//25-NullObject
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Log{
+	virtual void LogInfo(const string& message) const = 0;
+};
+
+struct ConsoleLog: Log{
+	void LogInfo(const string& message) const override{
+		cout << message << endl;
+	}
+};
+
+template <typename LogType>
+struct PaymentProcessing{
+	static_assert(is_base_of<Log, LogType>::value, "LogType must be a Log");
+
+	LogType log;
+
+	void Process(){
+		log.LogMessage("Processing payments...");
+	}
+};
+
+struct NullLog: Log{
+	void LogInfo(const string& message) const override {}
+};
+
+int main(){
+	PaymentProcessing<NullLog> pp;
+	getchar();
+	
+	return 0;
+}
